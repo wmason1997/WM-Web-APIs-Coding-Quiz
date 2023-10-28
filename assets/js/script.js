1ae9a5b9cbd0e073a0ceab2799e506e77f5ec52e
wmason1997 <williamcmason1997@gmail.com>
2023-10-28 12:53:31 -0700

    reconfigured question objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,48 @@
 //      choices: ["application programming interface", "attribute processing inadeqacy", "abstraction passkey indexing", "algorithm partition ideation"];
 //      answer: "application programming interface"
 // }
+const question_1 = {
+      question: "What does API stand for in coding?",
+      choices: ["application programming interface", "attribute processing inadeqacy", "abstraction passkey indexing", "algorithm partition ideation"],
+      answer: "application programming interface"
+};
 
-// Question 2
-// {
-//      question: "What does DOM stand for in coding?"
-//      choices: ["database operator matrix", "debugger origin mining", "deployment online mainframe", "document object model"];
-//      answer: "document object model"
-// }
+const question_2 = {
+
+      question: "What does DOM stand for in coding?",
+      choices: ["database operator matrix", "debugger origin mining", "deployment online mainframe", "document object model"],
+      answer: "document object model"
+ };
 
 // Question 3
-// {
-//      question: "What does JSON stand for in coding?"
-//      choices: ["jailbreak scalable offline network", "JavaScript Object Notation", "joystick super oscillation navigation", "jitter stabilization over normal"];
-//      answer: "JavaScript Object Notation"
-// }
 
-// Question 4
-// {
-//      question: "How do you implement timers?"
-//      choices: ["setClock() and clearClock()", "setTime() and clearTime()", "setInterval() and clearInterval()", "setDuration() and clearDuration()"];
-//      answer: "setInterval() and clearInterval()"
-// }
+const question_3 = {
 
-// Question 5
-// {
-//      question: "How do you handle click events?"
-//      choices: ["addClickHandler()", "addButtonEar()", "addPressSense()", "addEventListener()"];
-//      answer: "addEventListener()"
-// }
+      question: "What does JSON stand for in coding?",
+      choices: ["jailbreak scalable offline network", "JavaScript Object Notation", "joystick super oscillation navigation", "jitter stabilization over normal"],
+      answer: "JavaScript Object Notation"
+};
 
-// Need to reconfigure the above question objects
+const question_4 = {
+     question: "How do you implement timers?",
+     choices: ["setClock() and clearClock()", "setTime() and clearTime()", "setInterval() and clearInterval()", "setDuration() and clearDuration()"],
+     answer: "setInterval() and clearInterval()"
+}
+
+const question_5 = {
+
+     question: "How do you handle click events?",
+     choices: ["addClickHandler()", "addButtonEar()", "addPressSense()", "addEventListener()"],
+     answer: "addEventListener()"
+}
+
+
+
+var startQuiz = document.querySelector(".start-quiz-button");
+var timerElement = document. querySelector(".timer-count");
+
+
+// update highscores function
+// function updateHighscores() {
+//
+// }
