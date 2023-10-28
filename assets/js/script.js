045cb8c71dbf222c0b05571ccbc186101c9b76c0
wmason1997 <williamcmason1997@gmail.com>
2023-10-27 19:30:55 -0700

    Added 5 created questions to script.js file to be properly configured later.

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Question 1
+// {
+//      question: "What does API stand for in coding?";
+//      choices: ["application programming interface", "attribute processing inadeqacy", "abstraction passkey indexing", "algorithm partition ideation"];
+//      answer: "application programming interface"
+// }
+
+// Question 2
+// {
+//      question: "What does DOM stand for in coding?"
+//      choices: ["database operator matrix", "debugger origin mining", "deployment online mainframe", "document object model"];
+//      answer: "document object model"
+// }
+
+// Question 3
+// {
+//      question: "What does JSON stand for in coding?"
+//      choices: ["jailbreak scalable offline network", "JavaScript Object Notation", "joystick super oscillation navigation", "jitter stabilization over normal"];
+//      answer: "JavaScript Object Notation"
+// }
+
+// Question 4
+// {
+//      question: "How do you implement timers?"
+//      choices: ["setClock() and clearClock()", "setTime() and clearTime()", "setInterval() and clearInterval()", "setDuration() and clearDuration()"];
+//      answer: "setInterval() and clearInterval()"
+// }
+
+// Question 5
+// {
+//      question: "How do you handle click events?"
+//      choices: ["addClickHandler()", "addButtonEar()", "addPressSense()", "addEventListener()"];
+//      answer: "addEventListener()"
+// }
+
+// Need to reconfigure the above question objects
